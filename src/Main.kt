9973a8c90263da74a80e466041314e68bf423d4b
Kaloyan Nikolov <konik98@gmail.com>
2018-02-23 10:36:57 +0100

    Making the code easier to read

--- src/Main.kt
+++ src/Main.kt
@@ -8,15 +8,17 @@ import javax.swing.JPanel
 
 import kotlin.collections.HashMap
 
-val random = Random()
-fun rand(from: Int, to: Int): Int = random.nextInt(to - from) + from
 val numOfRows = 15
 var numberOfVerbs = 0
 
+//support functions for random and checking if verb is in an array
+fun rand(from: Int, to: Int): Int = Random().nextInt(to - from) + from
 fun notIn(item: Verb, allVerbs: Array<Verb?>): Boolean = (0 until allVerbs.size).none { item == allVerbs[it] }
 
+//keep reference of all the text fields
 var allTextFields: HashMap<Int, TextField> = HashMap()
 
+//add a row of three text inputs for the 3 forms of the verbs
 fun addRow(chosen: Int, first: String, second: String, third: String, frame: JFrame) {
     val verbFrame = JPanel(GridLayout(1, 3, 10, 5))
     verbFrame.preferredSize = Dimension(500, 20)
@@ -41,33 +43,33 @@ fun addRow(chosen: Int, first: String, second: String, third: String, frame: JFr
 }
 
 class VerbClass: ActionListener {
-    override fun actionPerformed(e: ActionEvent) {
-        if(e.source == resetButton){
-            VerbClass()
-        }
-        else if (e.source == checkButton){
-            for(i in 0..(numOfRows - 1)){
-                verbs.checkVerb(allTextFields[i*3], allTextFields[i*3 + 1], allTextFields[i*3 + 2], markVerb[i])
-            }
-            val mistakesFrame = JFrame ("Mistakes")
-            mistakesFrame.layout = GridLayout(verbs.mistakes.size,1)
-            mistakesFrame.setSize(500, verbs.mistakes.size * 30)
-            for (i in 0..(verbs.mistakes.size - 1)){
-                mistakesFrame.add(Label("   The right answer was: ${verbs.mistakes[i].pres}     ${verbs.mistakes[i].pret}     ${verbs.mistakes[i].part2}", Label.LEFT))
-            }
-            mistakesFrame.isVisible = true
-        }
+
+    //setup the windows with buttons
+    private val mainFrame = JFrame("Verb Test")
+
+    private val checkButton = JButton("Check")
+    private val resetButton = JButton("Reset") // will implement later
+
+    init {
+        mainFrame.layout = FlowLayout()
+
+        setVerbs()
+        resetButton.addActionListener(this)
+        checkButton.addActionListener(this)
+        mainFrame.add(checkButton)
+        //mainFrame.add(resetButton)
+
+        mainFrame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        mainFrame.setSize(540, numOfRows * 30 + 50)
+        mainFrame.isVisible = true
     }
 
-    val mainFrame = JFrame("Verb Test")
+    // setup the textboxes with verbs
+    private val verbs = VerbInit()
+    private var usedVerbs = arrayOfNulls<Verb>(numOfRows)
+    private var markVerb: HashMap<Int, String> = HashMap() // row , default
 
-
-
-    val verbs = VerbInit()
-    var usedVerbs = arrayOfNulls<Verb>(numOfRows)
-    var markVerb: HashMap<Int, String> = HashMap() // row , default
-
-    fun setVerbs() {
+    private fun setVerbs() {
         for (i: Int in 0..(numOfRows - 1)) {
             var currentVerb: Verb = verbs.getVerb()
             while (!notIn(currentVerb, usedVerbs)) {
@@ -89,29 +91,25 @@ class VerbClass: ActionListener {
         }
     }
 
-
-
-    val checkButton = JButton("Check")
-    val resetButton = JButton("Reset")
-
-    init {
-        mainFrame.layout = FlowLayout()
-
-        setVerbs()
-        resetButton.addActionListener(this)
-        checkButton.addActionListener(this)
-        mainFrame.add(checkButton)
-        //mainFrame.add(resetButton)
-
-        mainFrame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        mainFrame.setSize(540, numOfRows * 30 + 50)
-        //mainFrame.pack()
-        mainFrame.isVisible = true
-
+    override fun actionPerformed(e: ActionEvent) {
+        if(e.source == resetButton){
+            VerbClass()
+        }
+        else if (e.source == checkButton){
+            for(i in 0..(numOfRows - 1)){
+                verbs.checkVerb(allTextFields[i*3], allTextFields[i*3 + 1], allTextFields[i*3 + 2], markVerb[i])
+            }
+            val mistakesFrame = JFrame ("Mistakes")
+            mistakesFrame.layout = GridLayout(verbs.mistakes.size,1)
+            mistakesFrame.setSize(500, verbs.mistakes.size * 30)
+            for (i in 0..(verbs.mistakes.size - 1)){
+                mistakesFrame.add(Label("   The right answer was: ${verbs.mistakes[i].pres}     ${verbs.mistakes[i].pret}     ${verbs.mistakes[i].part2}", Label.LEFT))
+            }
+            mistakesFrame.isVisible = true
+        }
     }
 }
 
     fun main(args: Array<String>) {
         VerbClass()
     }
-
